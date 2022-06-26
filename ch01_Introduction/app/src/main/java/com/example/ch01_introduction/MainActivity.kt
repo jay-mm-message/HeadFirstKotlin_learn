fc68996016269e023f9cfdf35b61c09ee90cf97c
jcwang <jcwang@sonicwall.com>
2022-06-26 19:20:44 +0800

    Create ch01_Introduction and add test case.

---
+++ ch01_Introduction/app/src/main/java/com/example/ch01_introduction/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ch01_introduction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        p()
+    }
+
+    fun p() {
+        Log.e("ch01_introduction", "POW!")
+    }
+ }
